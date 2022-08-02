8a84ce4c7f2e02bc1dcdf2796a878363eed6db7d
Daviann4 <71868354+davianoh@users.noreply.github.com>
2022-08-02 12:46:15 +0700

    login

---
+++ Assets/Sprites/Login Sprite/ButtonInteraction.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    public Text userName;
+    public Text password;
+
+    public void buttonPressed()
+    {
+
+    }
+}
