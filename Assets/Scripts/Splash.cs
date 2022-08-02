b9927d23e067cbd96bbba5d9c889d1e6cfb7bdac
Muhammad Zufarrifqi Prakoso <zufarrifqi46@Gmail.com>
2022-08-02 11:24:12 +0700

    added splash

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+    [SerializeField] private float waitTime = 11f;
+
+    void Start () 
+    {
+        StartCoroutine(streamVideo());
+    }
+
+    private IEnumerator streamVideo()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene("Game");
+    }
+}
