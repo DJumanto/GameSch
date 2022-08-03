e300383a5ce8c3ea054e69e62cc8a21b3ca81208
Cahyadi Surya Nugraha <cahyadisn6@gmail.com>
2022-08-03 12:00:15 +0700

    merge branch timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,21 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using System;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] private int year;
+    [SerializeField] private int month;
+    [SerializeField] private int day;
     [SerializeField] private int hour;
     [SerializeField] private int minute;
     [SerializeField] TextMeshProUGUI countdownText;
 
     float currentTime = 0f;
-    float startingTime = (float)(new DateTime(2022, 8, 2) - DateTime.UtcNow).TotalSeconds - 25200f;
+    float startingTime;
 
     void Awake()
     {
+        startingTime = (float)((new DateTime(year, month, day) - DateTime.UtcNow).TotalSeconds - 25200f);
         startingTime += hour*3600 + minute*60;
 
         currentTime = startingTime;
@@ -29,7 +30,7 @@ public class Timer : MonoBehaviour
         if (currentTime <= 0){
             currentTime = 0;
         }
-         
+
         TimeSpan timeSpan = TimeSpan.FromSeconds(currentTime);
         string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
         countdownText.text = timeText; 
